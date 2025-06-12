80556bfec2cf674e8ed97b67250610719118bbb7
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2025-06-12 00:17:47 -0700

    Complete Chapter 2 guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a number guess between 1  and 100.\nType 'q' to quit.");
+    let rand_number = rand::rng().random_range(1..=100);
+    loop {
+        let mut guess_value = String::new();
+        io::stdin()
+            .read_line(&mut guess_value)
+            .expect("Failed to read stdin line input");
+        if guess_value.trim() == "q" {
+            break;
+        }
+        let guess_value: u32 = match guess_value.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess_value.cmp(&rand_number) {
+            Ordering::Less => println!("Too Low!"),
+            Ordering::Greater => println!("Too High!"),
+            Ordering::Equal => {
+                println!("You Win!\nThe number was {}", rand_number);
+                break;
+            }
+        }
+    }
 }
